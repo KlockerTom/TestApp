0a0a2fe2df53c19b9d419d25b34c78f0122c82a9
klock-th <klock-th@DDEMUCA2524A>
2017-03-13 11:35:01 +0100

    Add project files.

---
+++ TestApp/TestApp/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Unity;
+using TestApp.Views;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+		protected override void OnInitialized()
+		{
+			InitializeComponent();
+
+			NavigationService.NavigateAsync("NavigationPage/MainTabbedPage");
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<NavigationPage>();
+			Container.RegisterTypeForNavigation<MainTabbedPage>();
+			Container.RegisterTypeForNavigation<MySecondTabbedPage>();
+		}
+	}
+}
---
+++ TestApp/TestApp/ViewModels/MainTabbedPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestApp.ViewModels
+{
+	public class MainTabbedPageViewModel : BindableBase
+	{
+		INavigationService _navigationService;
+
+		public DelegateCommand NavigateToACommand { get; set; }
+		public MainTabbedPageViewModel(INavigationService navigationService)
+		{
+			//our prism services
+			_navigationService = navigationService;
+
+			NavigateToACommand = new DelegateCommand(NavigateToA);
+		}
+
+		void NavigateToA()
+		{
+			_navigationService.NavigateAsync("MySecondTabbedPage", useModalNavigation: true);
+		}
+	}
+}
